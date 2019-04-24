d5f23fc343068912638810fc97a7d98b3f569cec
hectorsvill <hectorstevenvillasano@gmail.com>
2019-04-24 14:49:31 -0700

    PaingtingViewController PaintingTableViewCell - Assign these to the controller and table view in Interface Builder using the identity inspector.

---
+++ Art-Gallery/Art-Gallery/PaintingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PaintingTableViewCell.swift
+//  Art-Gallery
+//
+//  Created by Hector Steven on 4/24/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class PaintingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
