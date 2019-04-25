9791cf17b0fd1c7c99fadd2efa5cd8f4112da450
hectorsvill <hectorstevenvillasano@gmail.com>
2019-04-24 23:16:21 -0700

    detailViewController

---
+++ Art-Gallery/Art-Gallery/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  Art-Gallery
+//
+//  Created by Hector Steven on 4/24/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+		
+    }
+    
+	@IBOutlet var paitingImageView: UIImageView!
+	var painting: Painting?
+	
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
